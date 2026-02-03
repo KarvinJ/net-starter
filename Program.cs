7bcd8995a36b76c19b3e07eab99a3ec1f98e83c4
knight <karvin.jimenez@alternard.com>
2026-02-03 17:26:58 -0400

    updating project to .net 10 and all its dependencies.

--- Program.cs
+++ Program.cs
@@ -15,7 +15,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
+    app.UseSwagger(options =>
+    {
+        options.SerializeAsV2 = true;
+    });
+    
     app.UseSwaggerUI();
 }
 
